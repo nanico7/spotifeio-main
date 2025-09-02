ee6cb3a0488a8483d37a944cac60866658d4b6f7
Thiago <neckelmachadothiago@gmail.com>
2025-09-02 20:59:06 -0300

    commit

---
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -0,0 +1,20 @@
+import {ChangeDetectionStrategy, Component, inject, OnInit} from "@angular/core";
+import { BotaoMenuComponent } from "./botao-menu/botao-menu.component";
+
+@Component({
+  selector: 'app-painel-esquerdo',
+  standalone: true,
+  imports: [BotaoMenuComponent],
+  templateUrl: './painel-esquerdo.component.html',
+  styleUrl: './painel-esquerdo.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PainelEsquerdoComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    
+  }
+
+  
+}
