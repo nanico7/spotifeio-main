dae2fb58b0379e0211acc0bd1a7b4090efc755ce
Thiago <neckelmachadothiago@gmail.com>
2025-09-02 18:51:09 -0300

    commit

--- src/app/pages/login/login.routes (2).ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./login.component').then(p => p.LoginComponent)
-    }
-]
